00bb62c769cb0112a342cf6a881e4dc174266dcb
Maxi <zafarswati520@gmail.com>
2024-12-02 22:28:11 +0500

    arch: mvvm

---
+++ lib/app/core/routes/type_safe_routes.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+
+
+final GlobalKey<NavigatorState> shellNavigatorKey1 =
+GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> shellNavigatorKey2 =
+GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> shellNavigatorKey3 =
+GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> shellNavigatorKey4 =
+GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> shellNavigatorKey5 =
+GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
+
+// @TypedGoRoute<SplashRoute>(path: '/')
+// class SplashRoute extends GoRouteData {
+//   const SplashRoute();
+//
+//   @override
+//   Widget build(BuildContext context, GoRouterState state) => const SplashPage();
+// }
+//
+// @TypedGoRoute<LoginRoute>(path: '/login')
+// class LoginRoute extends GoRouteData {
+//   const LoginRoute();
+//
+//   @override
+//   Widget build(BuildContext context, GoRouterState state) => const LoginPage();
+// }
+//
+//
+//
+
---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,328 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsFontsGen {
+  const $AssetsFontsGen();
+
+  /// File path: assets/fonts/ClashDisplay-Bold.otf
+  String get clashDisplayBold => 'assets/fonts/ClashDisplay-Bold.otf';
+
+  /// File path: assets/fonts/ClashDisplay-Extralight.otf
+  String get clashDisplayExtralight =>
+      'assets/fonts/ClashDisplay-Extralight.otf';
+
+  /// File path: assets/fonts/ClashDisplay-Light.otf
+  String get clashDisplayLight => 'assets/fonts/ClashDisplay-Light.otf';
+
+  /// File path: assets/fonts/ClashDisplay-Medium.otf
+  String get clashDisplayMedium => 'assets/fonts/ClashDisplay-Medium.otf';
+
+  /// File path: assets/fonts/ClashDisplay-Regular.otf
+  String get clashDisplayRegular => 'assets/fonts/ClashDisplay-Regular.otf';
+
+  /// File path: assets/fonts/ClashDisplay-Semibold.otf
+  String get clashDisplaySemibold => 'assets/fonts/ClashDisplay-Semibold.otf';
+
+  /// File path: assets/fonts/Montserrat-Black.ttf
+  String get montserratBlack => 'assets/fonts/Montserrat-Black.ttf';
+
+  /// File path: assets/fonts/Montserrat-Bold.ttf
+  String get montserratBold => 'assets/fonts/Montserrat-Bold.ttf';
+
+  /// File path: assets/fonts/Montserrat-ExtraBold.ttf
+  String get montserratExtraBold => 'assets/fonts/Montserrat-ExtraBold.ttf';
+
+  /// File path: assets/fonts/Montserrat-ExtraLight.ttf
+  String get montserratExtraLight => 'assets/fonts/Montserrat-ExtraLight.ttf';
+
+  /// File path: assets/fonts/Montserrat-Light.ttf
+  String get montserratLight => 'assets/fonts/Montserrat-Light.ttf';
+
+  /// File path: assets/fonts/Montserrat-Medium.ttf
+  String get montserratMedium => 'assets/fonts/Montserrat-Medium.ttf';
+
+  /// File path: assets/fonts/Montserrat-Regular.ttf
+  String get montserratRegular => 'assets/fonts/Montserrat-Regular.ttf';
+
+  /// File path: assets/fonts/Montserrat-SemiBold.ttf
+  String get montserratSemiBold => 'assets/fonts/Montserrat-SemiBold.ttf';
+
+  /// File path: assets/fonts/Montserrat-Thin.ttf
+  String get montserratThin => 'assets/fonts/Montserrat-Thin.ttf';
+
+  /// File path: assets/fonts/Poppins-Black.ttf
+  String get poppinsBlack => 'assets/fonts/Poppins-Black.ttf';
+
+  /// File path: assets/fonts/Poppins-Bold.ttf
+  String get poppinsBold => 'assets/fonts/Poppins-Bold.ttf';
+
+  /// File path: assets/fonts/Poppins-ExtraBold.ttf
+  String get poppinsExtraBold => 'assets/fonts/Poppins-ExtraBold.ttf';
+
+  /// File path: assets/fonts/Poppins-Light.ttf
+  String get poppinsLight => 'assets/fonts/Poppins-Light.ttf';
+
+  /// File path: assets/fonts/Poppins-Medium.ttf
+  String get poppinsMedium => 'assets/fonts/Poppins-Medium.ttf';
+
+  /// File path: assets/fonts/Poppins-Regular.ttf
+  String get poppinsRegular => 'assets/fonts/Poppins-Regular.ttf';
+
+  /// File path: assets/fonts/Poppins-SemiBold.ttf
+  String get poppinsSemiBold => 'assets/fonts/Poppins-SemiBold.ttf';
+
+  /// File path: assets/fonts/Poppins-Thin.ttf
+  String get poppinsThin => 'assets/fonts/Poppins-Thin.ttf';
+
+  /// List of all assets
+  List<String> get values => [
+        clashDisplayBold,
+        clashDisplayExtralight,
+        clashDisplayLight,
+        clashDisplayMedium,
+        clashDisplayRegular,
+        clashDisplaySemibold,
+        montserratBlack,
+        montserratBold,
+        montserratExtraBold,
+        montserratExtraLight,
+        montserratLight,
+        montserratMedium,
+        montserratRegular,
+        montserratSemiBold,
+        montserratThin,
+        poppinsBlack,
+        poppinsBold,
+        poppinsExtraBold,
+        poppinsLight,
+        poppinsMedium,
+        poppinsRegular,
+        poppinsSemiBold,
+        poppinsThin
+      ];
+}
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/Close.svg
+  String get close => 'assets/icons/Close.svg';
+
+  /// File path: assets/icons/Notification.svg
+  String get notification => 'assets/icons/Notification.svg';
+
+  /// File path: assets/icons/add.svg
+  String get add => 'assets/icons/add.svg';
+
+  /// File path: assets/icons/admin_note.svg
+  String get adminNote => 'assets/icons/admin_note.svg';
+
+  /// File path: assets/icons/approvals.svg
+  String get approvals => 'assets/icons/approvals.svg';
+
+  /// File path: assets/icons/attach_media.svg
+  String get attachMedia => 'assets/icons/attach_media.svg';
+
+  /// File path: assets/icons/back_arrow.svg
+  String get backArrow => 'assets/icons/back_arrow.svg';
+
+  /// File path: assets/icons/cash_control.svg
+  String get cashControl => 'assets/icons/cash_control.svg';
+
+  /// File path: assets/icons/check_file.svg
+  String get checkFile => 'assets/icons/check_file.svg';
+
+  /// File path: assets/icons/collection.svg
+  String get collection => 'assets/icons/collection.svg';
+
+  /// File path: assets/icons/collection_n_routes.svg
+  String get collectionNRoutes => 'assets/icons/collection_n_routes.svg';
+
+  /// File path: assets/icons/create.svg
+  String get create => 'assets/icons/create.svg';
+
+  /// File path: assets/icons/dashboard.svg
+  String get dashboard => 'assets/icons/dashboard.svg';
+
+  /// File path: assets/icons/database.svg
+  String get database => 'assets/icons/database.svg';
+
+  /// File path: assets/icons/file.svg
+  String get file => 'assets/icons/file.svg';
+
+  /// File path: assets/icons/group.svg
+  String get group => 'assets/icons/group.svg';
+
+  /// File path: assets/icons/notes.svg
+  String get notes => 'assets/icons/notes.svg';
+
+  /// File path: assets/icons/queue_item.svg
+  String get queueItem => 'assets/icons/queue_item.svg';
+
+  /// File path: assets/icons/sign.svg
+  String get sign => 'assets/icons/sign.svg';
+
+  /// File path: assets/icons/signup.svg
+  String get signup => 'assets/icons/signup.svg';
+
+  /// File path: assets/icons/supplier_n_order.svg
+  String get supplierNOrder => 'assets/icons/supplier_n_order.svg';
+
+  /// File path: assets/icons/three.svg
+  String get three => 'assets/icons/three.svg';
+
+  /// File path: assets/icons/users.svg
+  String get users => 'assets/icons/users.svg';
+
+  /// File path: assets/icons/work_order.svg
+  String get workOrder => 'assets/icons/work_order.svg';
+
+  /// List of all assets
+  List<String> get values => [
+        close,
+        notification,
+        add,
+        adminNote,
+        approvals,
+        attachMedia,
+        backArrow,
+        cashControl,
+        checkFile,
+        collection,
+        collectionNRoutes,
+        create,
+        dashboard,
+        database,
+        file,
+        group,
+        notes,
+        queueItem,
+        sign,
+        signup,
+        supplierNOrder,
+        three,
+        users,
+        workOrder
+      ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/Employee.svg
+  String get employeeSvg => 'assets/images/Employee.svg';
+
+  /// File path: assets/images/employee.png
+  AssetGenImage get employeePng =>
+      const AssetGenImage('assets/images/employee.png');
+
+  /// File path: assets/images/admin_pix.png
+  AssetGenImage get adminPix =>
+      const AssetGenImage('assets/images/admin_pix.png');
+
+  /// File path: assets/images/bg.png
+  AssetGenImage get bg => const AssetGenImage('assets/images/bg.png');
+
+  /// File path: assets/images/csr.png
+  AssetGenImage get csr => const AssetGenImage('assets/images/csr.png');
+
+  /// List of all assets
+  List<dynamic> get values => [employeeSvg, employeePng, adminPix, bg, csr];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsFontsGen fonts = $AssetsFontsGen();
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
